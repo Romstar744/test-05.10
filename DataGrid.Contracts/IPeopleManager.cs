ae2f2c13e6a3609e82c08732f61facc901c82b84
romst <romstar742@gmail.com>
2024-10-12 14:37:48 +0300

    DataGrid.Contracts

---
+++ DataGrid.Contracts/IPeopleManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp3.Model;
+
+namespace DataGrid.Contracts
+{
+	/// <summary>
+	/// dkfdskfkds
+	/// </summary>
+	public interface IPeopleManager
+	{
+		/// <summary>
+		/// Возвращает список Person
+		/// </summary>
+		/// <returns></returns>
+		IReadOnlyCollection<Person> GetAll();
+
+		Person Add(Person person);
+
+		void Edit (Person person);
+
+		void Delete (Guid id);
+	}
+}
